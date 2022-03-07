206b98fef64c8122a3366d7472307984f4f52e71
kdv <kdv200330@bk.ru>
2022-03-07 23:35:30 +0300

    profile alpha

--- src/js/api/userApi.js
+++ src/js/api/userApi.js
@@ -8,14 +8,13 @@ class User{
     firstName
     lastName
     birthday
-    coords
     city
     interests
     aboutUser
     userId
     gender
     async checkLogin(){
-        let response = await fetch('http://localhost:8080/user', {
+        let response = await fetch('http://localhost:8080/users', {
             method: "GET",
             credentials: "include"
         })
@@ -31,7 +30,7 @@ class User{
     }
     async logIn(){
         const request = JSON.stringify({email: this.email, password: this.password})
-        let response = await fetch('http://localhost:8080/user', {
+        let response = await fetch('http://localhost:8080/users', {
             method: "PUT",
             credentials: "include",
             body: request,
@@ -46,7 +45,7 @@ class User{
     }
     async logUp(){
         const request = JSON.stringify({email: this.email, password: this.password})
-        let response = await fetch('http://localhost:8080/user', {
+        let response = await fetch('http://localhost:8080/users', {
             method: "POST",
             credentials: "include",
             body: request,
@@ -61,11 +60,12 @@ class User{
     }
     async logOut(){
         const request = JSON.stringify({email: this.email, password: this.password})
-        let response = await fetch('http://localhost:8080/user', {
+        let response = await fetch('http://localhost:8080/users', {
             method: "DELETE",
             credentials: "include",
             body: request,
         })
+
         if(!response.ok){
             //TODO errors
             throw "Error"
@@ -73,19 +73,155 @@ class User{
         this.id = -1
         return this
     }
-    async longProfile(){
 
+    async getLongProfile(){
+        let response = await fetch('http://localhost:8080/profiles/' +  this.id.toString(),{
+            method: "GET",
+            credentials: "include",
+        })
+        if(response.status === 401){
+            return false
+        }
+        if(!response.ok){
+            throw false
+        }
+        const data = await response.json()
+        this.firstName = data.firstName
+        this.lastName = data.lastName
+        this.birthday = data.birthday
+        this.city  = data.city
+        this.interests = data.interests
+        this.userId = data.userId
+        this.gender = data.gender
+        this.aboutUser = data.aboutUser
+
+        return this
     }
-    async shortProfile(){
 
+    async shortProfile(){
+        let response = await fetch('http://localhost:8080/profiles/' + this.id.toString() + "/short",{
+            method: "GET",
+            credentials: "include",
+        })
+        if(response.status === 401){
+            return false
+        }
+        if(!response.ok){
+            throw false
+        }
+        const data = await response.json()
+        this.firstName = data.firstName
+        this.lastName = data.lastName
+        this.city  = data.city
+        return this
+    }
+
+    async changeProfile(User){
+        const request = JSON.stringify({firstName: User.firstName,
+            lastName: User.lastName,
+            birthday: User.birthday,
+            city: User.city,
+            userId: User.userId,
+            interests: User.interests,
+            aboutUser: User.aboutUser,
+            gender: User.gender});
+        alert(request)
+        let response = await fetch('http://localhost:8080/profiles/' + this.id.toString(),{
+            method: "PUT",
+            credentials: "include",
+            body: request,
+        })
+        if(!response.ok){
+            return false
+        }
+        this.firstName = User.firstName
+        this.lastName = User.lastName
+        this.birthday = User.birthday
+        this.city  = User.city
+        this.interests = User.interests
+        this.userId = User.userId
+        this.gender = User.gender
+        this.aboutUser = User.aboutUser
+        return this
     }
 }
 
 async function findCandidate(){
-    
+    let response = await fetch('http://localhost:8080/profiles/candidates',{
+        method: "POST",
+        credentials: "include",
+    })
+    if(!response.ok){
+        //TODO errors
+        throw "Error"
+    }
+    const data = await response.json()
+    let vector = data.candidates
+    return vector
 }
 
-let user = new User("jwie", "iewucw")
+
+let user = new User()
+
+function getProfile(){
+    user.getLongProfile().then(user =>{
+        alert("OK " + user.id + " " + user.aboutUser + " " + user.birthday + " " + user.lastName + " " + user.city)
+    }).catch(msg =>{
+        alert(msg)
+    })
+}
+
+function getShortProfile(){
+    user.shortProfile().then(user =>{
+        alert("OK " + user.id + " " + user.lastName)
+    }).catch(msg =>{
+        alert(msg)
+    })
+}
+
+function CandidateVector(){
+    findCandidate().then(users =>{
+        alert("OK " + users)
+    }).catch(msg =>{
+        alert(msg)
+    })
+}
+
+
+function userFromVector(){
+    findCandidate().then(vector => {
+        let user1 = new User()
+        user1.id = vector[0]
+        let user2 = new User()
+        user2.id = vector[1]
+        let user3 = new User()
+        user3.id = vector[2]
+        user1.getLongProfile().then(user => {
+            alert("OK " + user.aboutUser)
+        })
+        user2.getLongProfile().then(user => {
+            alert("OK " + user.aboutUser)
+        })
+        user3.getLongProfile().then(user => {
+            alert("OK " + user.aboutUser)
+        })
+    })
+}
+
+function changeProfile(){
+    let user23 = new User()
+    user23 = user
+    user23.lastName = "kdv"
+
+    user.changeProfile(user23).then(users =>{
+        alert("OK " + users.id + " " + users.lastName)
+    }).catch(msg =>{
+        alert(msg)
+    })
+}
+
+
+
 
 function auth(){
     user.checkLogin().then(bool =>{
@@ -109,6 +245,8 @@ function logUp(){
 }
 
 function logIn(){
+    user.password = "0"
+    user.email = "petrenko"
     user.logIn().then(user =>{
         alert("OK " + user.id)
     }).catch(msg =>{
