200954916e4e80c9e011a8abe4755a7eb7805130
antdenv <antdenv@ya.ru>
2022-03-07 18:39:45 +0300

    Validation

---
+++ public/globals.js
@@ -0,0 +1,12 @@
+const disableSW = false;
+
+const ajaxDebug = false;
+const modalsDebug = false;
+const fluxDebug = false;
+const routerDebug = false;
+const linkControllerDebug = false;
+const viewsDebug = false;
+const pageDebug = false;
+const wsDebug = false;
+
+export {disableSW, ajaxDebug, modalsDebug, fluxDebug, routerDebug, linkControllerDebug, viewsDebug, pageDebug, wsDebug};
