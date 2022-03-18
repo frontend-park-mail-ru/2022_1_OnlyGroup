40b5bdd7510e4c21447219b0bd37947d878e73f6
antdenv <antdenv@ya.ru>
2022-03-18 20:43:02 +0300

    fix

--- public/views/app/app.js
+++ public/views/app/app.js
@@ -8,6 +8,7 @@ import router from '../../router/router.js';
  */
 export class App {
   data;
+  vectorCandidates = [];
   /**
    * App page constructor
    * @param {Element} root
@@ -20,20 +21,31 @@ export class App {
     */
   async render() {
     this.getUserProfile();
-
     this.root.innerHTML = appComponent();
-    const name = this.data.FirstName + ' ' + this.data.LastName + ', ' + this.data.Birthday;
-    document.querySelector('.fullname').innerHTML = name;
-    document.querySelector('.info__city p').innerHTML = this.data.City;
+    this.innerUserInformation();
+    this.callInsertCandidate();
+    this.setHandlers();
+  }
 
-    this.vectorCandidates = [];
+  /**
+   * Async function for call insert candidates
+   * @returns
+   */
+  async callInsertCandidate() {
     try {
       await this.insertCandidate();
     } catch (e) {
       return;
     }
+  }
 
-    this.setHandlers();
+  /**
+   * Async function for inner user information to page
+   */
+  async innerUserInformation() {
+    const name = this.data.FirstName + ' ' + this.data.LastName + ', ' + this.data.Birthday;
+    document.querySelector('.fullname').innerHTML = name;
+    document.querySelector('.info__city p').innerHTML = this.data.City;
   }
 
   /**
@@ -48,6 +60,7 @@ export class App {
     }
     
     if (data === false) {
+      this.removeHandlers();
       router.go('/login');
       return;
     }
@@ -75,6 +88,7 @@ export class App {
   async insertCandidate() {
     const candidateId = await this.getCandidate();
     if (candidateId === -1) {
+      this.removeHandlers();
       router.go('/login');
     }
     const Candidate = await Api.getLongProfile(candidateId);
@@ -90,53 +104,103 @@ export class App {
   }
 
   /**
+   * Function for get DOM elements from page
+   * @returns {Array} elements
+   */
+  getDOMElements() {
+    const elements = [];
+    const editProfileBlocks = [];
+    editProfileBlocks.push(document.getElementById('navigation__buttons'));
+    editProfileBlocks.push(document.getElementById('navigation__field'));
+    editProfileBlocks.push(document.getElementById('navigation__profile'));
+    elements.push(document.getElementById('navigation__icons'));
+    elements.push(document.getElementById('settings__edit'));
+    elements.push(document.getElementById('cancel__button'));
+    elements.push(editProfileBlocks);
+    elements.push(document.getElementById('logout'));
+    
+    return elements;
+  }
+
+  /**
+   * Change event behavior for handlers
+   * @param {Event} event 
+   */
+  changeEventBehavior(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  /**
+   * Function for edit profile actions
+   * @param {Array} elements 
+   * @param {Array} styles 
+   * @param {Event} event 
+   */
+  editProfileActions(elements, styles, event) {
+    this.changeEventBehavior(event);
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.display = styles[i];
+    }
+  }
+
+  /**
+   * Logout function for handlers
+   * @returns 
+   */
+  async logoutButton(event) {
+    this.changeEventBehavior(event);
+    try {
+      const result = await Api.logOut();
+      if (!result) {
+        return;
+      }
+    } catch (event) {
+      return;
+    }
+
+    activeUser.logout();
+    router.go('/');
+    this.removeHandlers();
+  }
+
+  /**
    * Handlers for buttons on page
    */
   setHandlers() {
-    const buttons = document.getElementById('navigation__icons');
-    const editProfileButton = document.getElementById('settings__edit');
-    const cancelButton = document.getElementById('cancel__button');
-    const navigationButtons = document.getElementById('navigation__buttons');
-    const navigationField = document.getElementById('navigation__field');
-    const navigationProfile = document.getElementById('navigation__profile');
-    const logout = document.getElementById('logout');
+    const elements = this.getDOMElements();
     
-    buttons.addEventListener('click', (ev) =>{
-      ev.preventDefault();
-      ev.stopPropagation();
+    elements[0].addEventListener('click', (event) => {
+      this.changeEventBehavior(event);
       this.insertCandidate();
     });
 
-    editProfileButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      navigationButtons.style.display = 'none';
-      navigationField.style.display = 'none';
-      navigationProfile.style.display = 'flex';
+    elements[1].addEventListener('click', (event) => this.editProfileActions(elements[3], ['none', 'none', 'flex'], event));
+
+    elements[2].addEventListener('click', (event) => this.editProfileActions(elements[3], ['flex', 'flex', 'none'], event));
+
+    elements[4].addEventListener('click', async (event) => {
+      this.logoutButton(event);
+    });
+  }
+
+  /**
+   * Remove handlers from elements
+   */
+  removeHandlers() {
+    const elements = this.getDOMElements();
+
+    elements[0].addEventListener('click', (event) => {
+      this.changeEventBehavior(event);
+      this.insertCandidate();
     });
 
-    cancelButton.addEventListener('click', (event) => {
-      event.preventDefault();
-      event.stopPropagation();
-      navigationButtons.style.display = 'flex';
-      navigationField.style.display = 'flex';
-      navigationProfile.style.display = 'none';
-    });
+    elements[1].removeEventListener('click', (event) => this.editProfileActions(elements[3], ['none', 'none', 'flex'], event));
 
-    logout.addEventListener('click', async (event) =>{
-      event.preventDefault();
-      event.stopPropagation();
-      try {
-        const result = await Api.logOut();
-        if (!result) {
-          return;
-        }
-      } catch (event) {
-        return;
-      }
+    elements[2].removeEventListener('click', (event) => this.editProfileActions(elements[3], ['flex', 'flex', 'none'], event));
 
-      activeUser.logout();
-      router.go('/');
+    elements[4].addEventListener('click', async (event) => {
+      this.logoutButton(event);
     });
   }
 }
