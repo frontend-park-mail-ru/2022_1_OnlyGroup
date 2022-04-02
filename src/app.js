d2b7520b43c0b5c0aa8c8fe9d77c95b73e69a807
antdenv <antdenv@ya.ru>
2022-04-02 16:47:27 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -6,11 +6,7 @@ import { LayoutComponent } from './Components/Layout/Layout.js';
 import { LogoComponent } from './Components/Logo/Logo.js';
 import { MessageComponent } from './Components/Message/Message.js';
 import { TextWithIconComponent } from './Components/TextWithIcon/TextWithIcon.js';
+import EventBus from './Modules/EventBus.js';
 import './style.scss';
 
 const root = document.getElementById('root');
-
-root.innerHTML = TextWithIconComponent.render({
-    iconSrc: './static/images/CardsDark.png',
-    textContent: 'Hello!'
-});
